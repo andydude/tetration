d845233c36a2fbd2bba458acd11e17cf9dc9f374
Andrew Robbins <and_j_rob@yahoo.com>
2015-12-22 20:59:45 -0500

    Added sage_tetration.py

--- dl/sage_tetration.py
+++ dl/sage_tetration.py
@@ -34,19 +34,33 @@ def simplify_series(ser):
     #    ser._unsafe_mutate(ck, canonicalize(coeff))
     #return ser
 
+# the Carleman matrix
 def carleman_matrix(expr, x_x0, order=10, ring=SR):
     x, x0 = x_x0
     order = int(order)
-    return matrix([[ring(diff(expr**j, x, k).subs(**{(str(x)): x0}) -
-                         kronecker_delta(j, k)*factorial(k))
+    return matrix([[ring(diff(expr**j, x, k).subs(**{(str(x)): x0})/factorial(k) -
+                         kronecker_delta(j, k))
                     for k in range(0, order + 1)]
                     for j in range(0, order + 1)])
+
+# the Abel matrix
+def abel_matrix(expr, x_x0, order=10, ring=SR):
+    x, x0 = x_x0
+    order = int(order)
+    m = transpose(carleman_matrix(expr, x_x0, order=order, ring=ring))
+    return matrix([[m[j][k]
+                    for k in range(1, order + 1)]
+                    for j in range(0, order)])
 
 # variant of the Abel matrix
 def superlog_matrix(log_base, order=SUPERLOG_ORDER, ring=SUPERLOG_RING):
     order = int(order)
-    return matrix([[ring((Integer(k)**Integer(j)) -
-                         kronecker_delta(Integer(j), Integer(k))*factorial(Integer(j))/((log_base)**Integer(j)))
+    def elem(j, k):
+        j = Integer(j)
+        k = Integer(k)
+        return ((k**j)*(log_base**j)/factorial(j) -
+                kronecker_delta(j, k))
+    return matrix([[ring(elem(j, k))
                     for k in range(1, order + 1)]
                     for j in range(0, order)])
 
@@ -70,8 +84,9 @@ def superlog_series(log_base, z, order=SUPERLOG_ORDER, ring=SUPERLOG_RING, cache
     coeff = cache[key_base][order]
     
     # N-ary summation
+    ser = sum([c*z**(i+1) for i, c in enumerate(coeff)]) - 1
     try:
-        ser = sum([c*z**(i+1) for i, c in enumerate(coeff)]) - 1 + z.O(order + 1)
+        ser += z.O(order + 1)
     except:
-        ser = sum([c*z**(i+1) for i, c in enumerate(coeff)]) - 1
+        pass
     return ser
